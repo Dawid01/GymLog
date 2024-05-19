1af07cdbe6e073b210a993755ca49a8bc22d78fb
Dawid Szczepaniak <dawidszczepaniak55@gmail.com>
2024-05-19 17:54:11 +0200

    changes

--- GymLog/app/src/main/java/com/szczepaniak/dawid/gymlog/activities/CreateRoutineActivity.kt
+++ GymLog/app/src/main/java/com/szczepaniak/dawid/gymlog/activities/CreateRoutineActivity.kt
@@ -1,6 +1,9 @@
 package com.szczepaniak.dawid.gymlog.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +20,19 @@ class CreateRoutineActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val addButton: Button = findViewById(R.id.add_exercise_button)
+        addButton.setOnClickListener{
+            val intent= Intent(this, ExcercisesActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val cancel: TextView = findViewById(R.id.cancel_text)
+        cancel.setOnClickListener{
+            finish()
+        }
+
     }
 }
